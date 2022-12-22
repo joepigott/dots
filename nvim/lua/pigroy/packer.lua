6c35e477dc603661f8093fd50cc96105b6d82c1e
Joe Pigott <joe.pigott17@gmail.com>
2022-12-21 18:02:50 -0700

    added nvim

---
+++ nvim/lua/pigroy/packer.lua
@@ -0,0 +1,35 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+    use "wbthomason/packer.nvim"
+    use "Shatur/neovim-ayu"
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons',
+        },
+    }
+    use 'neovim/nvim-lspconfig'
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'saadparwaiz1/cmp_luasnip'
+    use {
+        'L3MON4D3/LuaSnip',
+        tag = "v<CurrentMajor>.*"
+    }
+    use 'rafamadriz/friendly-snippets'
+    use 'windwp/nvim-autopairs'
+    use 'lukas-reineke/indent-blankline.nvim'
+    use 'norcalli/nvim-colorizer.lua'
+    use 'NvChad/nvterm'
+end)
